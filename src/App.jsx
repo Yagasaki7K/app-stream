80f6739cef60d9ca4ce21e6a9bf1b5aef59c412c
Anderson Marlon <anderson18.marlon@gmail.com>
2024-05-02 11:15:47 -0300

    Added: Chat + Default Stream is Gaules - 02-05 11:15

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,49 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styled from "styled-components"
 
 function App() {
     const [inputChannel, setInputChannel] = useState('')
-    const [channel, setChannel] = useState('todo_mundo_ama_br')
+    const [channel, setChannel] = useState('gaules')
+    const [isChat, setIsChat] = useState(false)
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    })
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+
+        if (typeof window !== 'undefined') {
+            window.addEventListener('resize', handleResize);
+            handleResize();
+
+            return () => window.removeEventListener('resize', handleResize);
+        }
+    }, []);
 
     const isProd = 'thetwitch.vercel.app'
+    // const isProd = 'localhost'
 
     return (
         <AppDetails>
-            <div className="overflow" />
+            <div className="overlay" />
             <div className="header">
                 <input type="text" placeholder="Insert the stream channel" onChange={e => setInputChannel(e.target.value)} />
                 <button onClick={() => setChannel(inputChannel)}>Search Channel</button>
+                {windowSize && windowSize.width > 1024 ? <button onClick={() => setIsChat(!isChat)}>Show Chat</button> : null}
             </div>
             <div className="player">
                 <iframe id="twitchPlayer" src={"https://player.twitch.tv/?channel=" + channel + "&muted=true&parent=" + isProd} allowFullScreen autoPlay />
+
+                {isChat ?
+                    <iframe id="twitchChat" className="chat" src={"https://www.twitch.tv/embed/" + channel + "/chat?parent=" + isProd} allowFullScreen />
+                    : null
+                }
             </div>
             <div className="copyright">
                 ©&nbsp;2024 - {new Date().getFullYear()}&nbsp;<a href="https://github.com/Yagasaki7K/app-stream" target="_blank" rel="noreferrer">app-stream</a>&nbsp;by&nbsp;<a href="https://yagasaki.dev" target="_blank" rel="noreferrer">Yagasaki7K</a>&nbsp;<span>|</span>&nbsp;Todas as imagens são marcas registradas dos seus respectivos proprietários
@@ -59,8 +87,9 @@ const AppDetails = styled.div`
         button {
             height: 50px;
             font-size: 18px;
+            background: var(--purple);
             color: var(--font-light);
-            background-color: transparent;
+            margin-right: 1rem;
 
             &:hover {
                 cursor: pointer;
@@ -82,11 +111,23 @@ const AppDetails = styled.div`
         display: flex;
         justify-content: center;
         align-items: center;
+        padding: 0 2rem;
 
         iframe {
             width: 90%;
             height: 51.4rem;
             border: none;
+
+            @media (max-width: 768px) {
+                height: 30rem;
+            }
+        }
+
+        .chat {
+            width: 20%;
+            height: 51.4rem;
+            border: none;
+            margin-left: 1rem;
 
             @media (max-width: 768px) {
                 height: 30rem;
